19d09d3e4fa91003e2872a484b741fbd9a026290
Guille <guillermo.barrasa@enti.cat>
2021-05-22 18:03:55 +0200

    Añadidos bricks y particulas de fuego

---
+++ Assets/Scripts/EarthBrick.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EarthBrick : Brick
+{
+
+    public Rigidbody2D rb2d;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    protected override void HitBehaviour()
+    {
+        rb2d.bodyType = RigidbodyType2D.Dynamic;
+        rb2d.mass = 100;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Lose")
+        {
+            Destroy(this);
+        }
+        if(collision.gameObject.tag == "Block")
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+
+}
---
+++ Assets/Scripts/FireBrick.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBrick : Brick
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    protected override void HitBehaviour()
+    {
+       
+    }
+
+}
