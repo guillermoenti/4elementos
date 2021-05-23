cbf3289b58fcc3ff553a6860739b1d2ef466970d
Guille <guillermo.barrasa@enti.cat>
2021-05-23 19:08:52 +0200

    Bloque de tierra done

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+    const int maxLives = 3;
+    public int lives;
+
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(this);
+        }
+        else
+        {
+            Debug.Log("Warning: multiple " + this + " in scene!");
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lives = 3;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GetHit()
+    {
+        lives--;
+    }
+
+    public void GetHealed()
+    {
+        if(lives < maxLives)
+        {
+            lives++;
+        }
+    }
+}
