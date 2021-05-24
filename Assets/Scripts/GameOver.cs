7042b1c0b22dece23cd0389b19159e25e8ad3bd1
Pedrofenti <pedro.fernandez@enti.cat>
2021-05-24 12:53:44 +0200

    Win y Game Over Scenes

--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -2,10 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameOver : MonoBehaviour
 {
     [SerializeField] GameObject Particles;
+    [SerializeField] Text hehe;
+
+    bool is_hehe;
+    float timer;
+
+    private void Start()
+    {
+        hehe.enabled = false;
+        is_hehe = false;
+        timer = 0;
+    }
+
+    private void Update()
+    {
+        if (is_hehe)
+        {
+            timer += Time.deltaTime;
+            if (timer > 2)
+            {
+                is_hehe = false;
+                hehe.enabled = false;
+                timer = 0;
+            }
+        }
+    }
 
     public void OnClick()
     {
@@ -14,6 +40,9 @@ public class GameOver : MonoBehaviour
 
     public void Sike()
     {
-        Particles.GetComponent<ParticleSystem>().Play();
+        Instantiate(Particles, new Vector3(0, 0, 0), Quaternion.Euler(0, 0, 0));
+
+        is_hehe = true;
+        hehe.enabled = true;
     }
 }
